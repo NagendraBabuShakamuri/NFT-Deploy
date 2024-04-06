9eae5507d2683ae131bfd5aeddce380c4287681d
NagendraBabuShakamuri <shakamuri.n@northeastern.edu>
2024-04-05 21:54:26 -0400

    Added code to deploy the contract.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider } from 'wagmi'
+import { config } from './config'
+import { ConnectWallet } from './ConnectWallet';
+import DeployContract from "./deployContract";
+
+// 2. Set up a React Query client.
+const queryClient = new QueryClient()
 
 function App() {
+  const [nftName, setNftName] = useState<string>('');
+  const [nftSymbol, setNftSymbol] = useState<string>('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectWallet />
+        <div>
+          <input
+            type="text"
+            value={nftName}
+            onChange={(e) => setNftName(e.target.value)}
+            placeholder="NFT Collection Name"
+          />
+          <input
+            type="text"
+            value={nftSymbol}
+            onChange={(e) => setNftSymbol(e.target.value)}
+            placeholder="NFT Symbol"
+          />
+        </div>
+        <DeployContract
+          contractArg1={nftName} 
+          contractArg2={nftSymbol}
+        />
+      </QueryClientProvider> 
+    </WagmiProvider>
   );
 }
+
 
 export default App;
